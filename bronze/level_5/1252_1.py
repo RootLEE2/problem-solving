ecf432b3d02ee0307eb8f009e03a7cbbf44c37b2
RootLEE2 <ro_otcom4@naver.com>
2021-08-12 19:59:48 +0900

    1252 python

---
+++ bronze/level_5/1252_1.py
@@ -0,0 +1,24 @@
+A,B = input().split()
+if len(A) > len(B):
+  B = '0'*(len(A)-len(B)) + B
+else:
+  A = '0'*(len(B)-len(A)) + A
+
+result = ''
+count = 0
+for i in range (len(A)):
+  temp = count + int(A[-(i+1)]) + int(B[-(i+1)])
+  if temp > 1:
+    count = 1
+    result = str(temp%2) + result
+  else:
+    count = 0
+    result = str(temp%2) + result
+if count == 1:
+  result = '1' + result
+
+if '1' not in result:
+  print('0')
+else:
+  temp = result.index('1')
+  print(result[temp:])
