5c161602d156f0fa499c0b58e68f5f01e1f14910
RootLEE2 <ro_otcom4@naver.com>
2021-08-31 10:53:39 +0900

    2667 python

--- silver/level_1/2667.py
+++ silver/level_1/2667.py
@@ -1,4 +1,4 @@
-# ver.1 
+# ver.1  76ms
 def dfs(x,y):
   if x < 0 or y < 0 or N <= x or N <= y:
     return 0
@@ -20,35 +20,41 @@ for i in range(N):
       cnt.append(dfs(i,j))
 
 cnt.sort()
-num = len(cnt)
-print(num)
-for i in range(num):
-  print(cnt[i])
+print(len(cnt))
+for element in cnt:
+  print(element)
 
 
-# ver.2
+# ver.2  68ms
 N = int(input())
-graph = []; cnt = 0; 
+graph = []
+cnt = 0; cntList = []; 
 for _ in range(N):
   graph.append(list(map(int, input())))
 
 def dfs(x,y):
   if x < 0 or y < 0 or N <= x or N <= y:
-    return 0
+    return
   if graph[x][y] == 0:
-    return 0
+    return
 
+  global cnt
+  cnt += 1
   graph[x][y] = 0
-  return dfs(x+1,y) + dfs(x-1,y) + dfs(x,y+1) + dfs(x,y-1) + 1
 
-cnt = []
+  dfs(x+1,y)
+  dfs(x-1,y)
+  dfs(x,y+1)
+  dfs(x,y-1)
+
 for i in range(N):
   for j in range(N):
     if graph[i][j] == 1:
-      cnt.append(dfs(i,j))
+      dfs(i,j)
+      cntList.append(cnt)
+      cnt = 0
 
-cnt.sort()
-num = len(cnt)
-print(num)
-for i in range(num):
-  print(cnt[i])
+cntList.sort()
+print(len(cntList))
+for element in cntList:
+  print(element)
