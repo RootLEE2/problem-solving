4e07002134de2c488a094d39c64e03d4e944ea76
RootLEE2 <ro_otcom4@naver.com>
2021-09-13 19:56:31 +0900

    1890 python

--- silver/level_2/1890.py
+++ silver/level_2/1890.py
@@ -1,27 +1,27 @@
 # ver.1  80ms
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-# N = int(input())
-# board = [list(map(int, input().split())) for _ in range(N)]
-# dp = [[0]*N for _ in range(N)]; dp[0][0] = 1; 
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0]*N for _ in range(N)]; dp[0][0] = 1; 
 
-# for i in range(N):
-#   for j in range(N):
-#     if i == N-1 and j == N-1:
-#       break
-#     if dp[i][j] == 0:
-#       continue
+for i in range(N):
+  for j in range(N):
+    if i == N-1 and j == N-1:
+      break
+    if dp[i][j] == 0:
+      continue
     
-#     jump = board[i][j]
-#     if i+jump < N:
-#       dp[i+jump][j] += dp[i][j]
-#     if j+jump < N:
-#       dp[i][j+jump] += dp[i][j]
-# print(dp[N-1][N-1])
+    jump = board[i][j]
+    if i+jump < N:
+      dp[i+jump][j] += dp[i][j]
+    if j+jump < N:
+      dp[i][j+jump] += dp[i][j]
+print(dp[N-1][N-1])
 
 
-# ver.2
+# ver.2  메모리 초과
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -32,25 +32,25 @@ def bfs():
 
   while queue:
     x,y = queue.popleft()
-    for i in range(8):
-      nx = x + board[x][y] * dx[i]
-      ny = y + dy[i]
+    for i in range(2):
+      nx = x + jump[x][y] * dx[i]
+      ny = y + jump[x][y] * dy[i]
 
-      if nx < 0 or ny < 0 or w <= nx or h <= ny:
+      if nx < 0 or ny < 0 or N <= nx or N <= ny:
         continue
-      if board[ny][nx] == 0:
+      if jump[ny][nx] == 0:
+        board[ny][nx] += board[x][y]
         continue
-      if board[ny][nx] == 1:
-        board[ny][nx] = 0
-        queue.append((ny,nx))
+      else:
+        board[nx][ny] += board[x][y]
+        queue.append((nx,ny))
 
 dx = [1, 0]
 dy = [0, 1]
         
 N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
-dp = [[0]*N for _ in range(N)]; dp[0][0] = 1; 
+jump = [list(map(int, input().split())) for _ in range(N)]
+board = [[0]*N for _ in range(N)]; board[0][0] = 1; 
 
-cnt = 0
 bfs()
-print(cnt)
+print(board[N-1][N-1])
