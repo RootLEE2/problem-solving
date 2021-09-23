243fc0869be854c312f28ef18dfd8e4dc73bab36
RootLEE2 <ro_otcom4@naver.com>
2021-09-23 20:05:46 +0900

    1011 cpp

---
+++ gold/level_5/1011.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int T; cin >> T;
+  int x, y, dist, distTmp, cnt;
+  while (T--) {
+    cin >> x >> y;
+    dist = y - x;
+    distTmp = sqrt(dist);
+    cnt = pow(distTmp,2) == dist ? 2*distTmp - 1 : 2*distTmp;
+    if (dist > distTmp * (distTmp+1)) { cnt ++; }
+    cout << cnt << '\n';
+  }
+}
