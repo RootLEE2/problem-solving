127df71a5499d6101eaa5819b846449d1c4848e3
root2 <2rootcom4@gmail.com>
2022-04-29 18:06:46 +0900

    feat: Add cpp code frame

---
+++ cpp_frame.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+#define MAX_M 300000
+#define MAX_LENGTH 100001
+#define INF 0x3f3f3f3f
+#define SQUARE(x) ((x) * (x))
+
+void solve() {
+  // solution part. 
+}
+
+void input() {
+  // input part. 
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+
+  input();
+  solve();
+
+  return 0;
+}
