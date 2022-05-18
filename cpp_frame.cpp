a1b88fd6809c3970274cb9d9a326d50a986a9a5d
root2 <2rootcom4@gmail.com>
2022-05-18 09:43:14 +0900

    feat: add log2 value & modular in cpp frame

--- cpp_frame.cpp
+++ cpp_frame.cpp
@@ -11,10 +11,12 @@ using namespace std;
 #define NUM_OF_ALPHABET 26
 #define INF 0x3f3f3f3f
 #define SQUARE(x) ((x) * (x))
+#define MOD(x) ((x) % 1000000007)
 typedef long long ll;
 // log2(100000) == 16.609xxx
 // log2(200000) == 17.609xxx
 // log2(500000) == 18.931xxx
+// log2(1000000) == 19.931xxx
 
 void solve() {
   // solution part. 
