700c3c3cbbe972c243edeb73eef8538398cddd5e
2root <2rootcom4@gmail.com>
2022-05-23 09:13:11 +0900

    feat: 14438.cpp in beakjoon/g1

---
+++ baekjoon/3_gold/level_1/cpp/14438.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+#define MAX_N 100005
+#define MAX_LENGTH 100005
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+#define SQUARE(x) ((x) * (x))
+#define MOD(x) ((x) % 1000000007)
+typedef long long ll;
+// log2(100000) == 16.609xxx
+// log2(200000) == 17.609xxx
+// log2(500000) == 18.931xxx
+// log2(1000000) == 19.931xxx
+
+int N, M;
+int A[MAX_N];
+
+int segTree[1 << 18];  // int segTree[4 * MAX_N];
+
+int min_queries(int node, int tl, int tr, int l, int r) {
+  if (r < l) return INF;
+  if (tl == l && tr == r) return segTree[node];
+
+  int doubleNode = node << 1;  // int doubleNode = 2 * node;
+  int tm = (tl + tr) >> 1;  // int tm = (tl + tr) / 2;
+
+  int leftChild = min_queries(doubleNode, tl, tm, l, min(tm, r));
+  int rightChild = min_queries(doubleNode + 1, tm + 1, tr, max(tm + 1, l), r);
+
+  return min(leftChild, rightChild);
+}
+
+void update_query(int node, int tl, int tr, int idx, int newVal) {
+  if (idx < tl || tr < idx) return;
+
+  if (tl == tr) {
+    segTree[node] = newVal;
+    return;
+  }
+
+  int doubleNode = node << 1;  // int doubleNode = 2 * node;
+  int tm = (tl + tr) >> 1;  // int tm = (tl + tr) / 2;
+
+  update_query(doubleNode, tl, tm, idx, newVal);
+  update_query(doubleNode + 1, tm + 1, tr, idx, newVal);
+
+  segTree[node] = min(segTree[doubleNode], segTree[doubleNode + 1]);
+}
+
+void build_seg_tree(int node, int tl, int tr) {
+  if (tl == tr) {
+    segTree[node] = A[tl];
+    return;
+  }
+
+  int doubleNode = node << 1;  // int doubleNode = 2 * node;
+  int tm = (tl + tr) >> 1;  // int tm = (tl + tr) / 2;
+
+  build_seg_tree(doubleNode, tl, tm);
+  build_seg_tree(doubleNode + 1, tm + 1, tr);
+
+  segTree[node] = min(segTree[doubleNode], segTree[doubleNode + 1]);
+}
+
+void solve() {
+  build_seg_tree(1, 1, N);
+
+  for (int i = 0; i < M; i++) {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == 1) {
+      update_query(1, 1, N, b, c);
+    }
+    else if (a == 2) {
+      cout << min_queries(1, 1, N, b, c) << '\n';
+    }
+  }
+}
+
+void input() {
+  cin >> N;
+
+  for (int i = 1; i <= N; i++) {
+    cin >> A[i];
+  }
+
+  cin >> M;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+
+  input();
+  solve();
+
+  return 0;
+}
