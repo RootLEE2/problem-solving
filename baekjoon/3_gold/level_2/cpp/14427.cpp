9db7c2cb84066db8aba931949c03b993ee1ed7f5
2root <2rootcom4@gmail.com>
2022-05-24 21:40:43 +0900

    feat: 14427.cpp in baekjoon/g2

---
+++ baekjoon/3_gold/level_2/cpp/14427.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+#define MAX_N 1003
+#define MAX_LENGTH 100005
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+#define SQUARE(x) ((x) * (x))
+#define MOD(x) ((x) % 1000000007)
+typedef long long ll;
+// log2(100000) == 16.609xxx
+// log2(200000) == 17.609xxx
+// log2(500000) == 18.931xxx
+// log2(1000000) == 19.931xxx
+
+int N, M;
+int A[MAX_N];
+
+int segTree[1 << 18];  // int segTree[4 * MAX_N];
+
+void update_query(int node, int tl, int tr, int idx, int newVal) {
+  if (idx < tl || tr < idx) return;
+  if (tl == tr) {
+    segTree[node] = tl;
+    return;
+  }
+
+  int doubleNode = node << 1;  // int doubleNode = 2 * node;
+  int tm = (tl + tr) >> 1;  // int tm = (tl + tr) / 2;
+
+  update_query(doubleNode, tl, tm, idx, newVal);
+  update_query(doubleNode + 1, tm + 1, tr, idx, newVal);
+
+  if (A[segTree[doubleNode]] <= A[segTree[doubleNode + 1]]) {
+    segTree[node] = segTree[doubleNode];
+  } else {
+    segTree[node] = segTree[doubleNode + 1];
+  }
+}
+
+void build_seg_tree(int node, int tl, int tr) {
+  if (tl == tr) {
+    segTree[node] = tl;
+    return;
+  }
+
+  int doubleNode = node << 1;  // int doubleNode = 2 * node;
+  int tm = (tl + tr) >> 1;  // int tm = (tl + tr) / 2;
+
+  build_seg_tree(doubleNode, tl, tm);
+  build_seg_tree(doubleNode + 1, tm + 1, tr);
+
+  if (A[segTree[doubleNode]] <= A[segTree[doubleNode + 1]]) {
+    segTree[node] = segTree[doubleNode];
+  } else {
+    segTree[node] = segTree[doubleNode + 1];
+  }
+}
+
+void solve() {
+ build_seg_tree(1, 1, N);
+  
+  for (int m = 0; m < M; m++) {
+    int c;
+    cin >> c;
+
+    if (c == 1) {
+      int i, v;
+      cin >> i >> v;
+
+      A[i] = v;
+      update_query(1, 1, N, i, v);
+    }
+    else if (c == 2) {
+      cout << segTree[1] << '\n';
+    }
+  }
+}
+
+void input() {
+  cin >> N;
+
+  for (int i = 1; i <= N; i++) {
+    cin >> A[i];
+  }
+
+  cin >> M;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+
+  input();
+  solve();
+
+  return 0;
+}
