010012577bc8322cc11670f633b03bb95b5953a4
2root <2rootcom4@gmail.com>
2022-06-17 16:34:34 +0900

    feat: 1669H.cpp in codeforces/greedy

---
+++ codeforces/greedy/cpp/1669H.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+#define MAX_N 200005
+#define MAX_LENGTH 100005
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+#define SQUARE(x) ((x) * (x))
+#define MOD(x) ((x) % 1000000007)
+typedef long long ll;
+// log2(100000) == 16.609xxx
+// log2(200000) == 17.609xxx
+// log2(500000) == 18.931xxx
+// log2(1000000) == 19.931xxx
+
+int n, k;
+int oneCnt[31];
+
+void solve() {
+  int maxValue = 0;
+
+  for (int i = 30; i >= 0; i--) {
+    if (n - oneCnt[i] <= k) {
+      k -= n - oneCnt[i];
+      maxValue += 1 << i;
+    }
+  }
+
+  cout << maxValue << '\n';
+}
+
+void input() {
+  cin >> n >> k;
+
+  for (int i = 1; i <= n; i++) {
+    int a_i;
+    cin >> a_i;
+
+    for (int j = 0; a_i != 0; j++, a_i >>= 1) {
+      oneCnt[j] += a_i & 1;
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+
+  int t;
+  for (cin >> t; t > 0; t--) {
+    memset(oneCnt, 0, sizeof(oneCnt));
+
+    input();
+    solve();
+  }
+
+  return 0;
+}
