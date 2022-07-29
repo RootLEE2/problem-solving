5215619a3ca0ca2ed440939aa695ca4397a0f53f
2root <2rootcom4@gmail.com>
2022-07-29 20:59:13 +0900

    feat: 1008.cpp in baekjoon/b5

---
+++ baekjoon/1_bronze/level_5/cpp/1008.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MAX_N 102
+#define MAX_LENGTH 102
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+#define SQUARE(x) ((x) * (x))
+#define MOD 1000000007
+typedef long long ll;
+// log2(100000) == 16.609xxx
+// log2(200000) == 17.609xxx
+// log2(500000) == 18.931xxx
+// log2(1000000) == 19.931xxx
+
+double A, B;
+
+void solve() {
+  cout.precision(10);
+  cout << fixed;
+  cout << A / B;
+}
+
+void input() {
+  cin >> A >> B;
+}
+
+int main() {
+  fastio;
+  
+  input();
+  solve();
+  
+  return 0;
+}
