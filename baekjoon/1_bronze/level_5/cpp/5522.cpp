c793c390d6c714a8b53d04a129404d0114ba4c40
2root <2rootcom4@gmail.com>
2022-08-21 20:06:35 +0900

    feat: 5522.cpp in baekjoon/b5

---
+++ baekjoon/1_bronze/level_5/cpp/5522.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MAX_N 302
+#define MAX_LENGTH 5003 >> 1
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+#define SQUARE(x) ((x) * (x))
+#define MOD 1000000007
+typedef long long ll;
+// log2(100000) == 16.609xxx
+// log2(200000) == 17.609xxx
+// log2(500000) == 18.931xxx
+// log2(1000000) == 19.931xxx
+
+int sum;
+
+void solve() {
+  cout << sum;
+}
+
+void input() {
+  for (int i = 0; i < 5; i++) {
+    int n; cin >> n;
+    sum += n;
+  }
+}
+
+int main() {
+  fastio;
+
+  input();
+  solve();
+
+  return 0;
+}
