744b5b08d2cb58ef817cb4be05375ff8afd4adeb
2root <2rootcom4@gmail.com>
2022-08-21 19:28:24 +0900

    feat: 9653.cpp in baekjoon/b5

---
+++ baekjoon/1_bronze/level_5/cpp/9653.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MAX_N 302
+#define MAX_LENGTH 5003 >> 1
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+#define SQUARE(x) ((x) * (x))
+#define MOD 1000000007
+typedef long long ll;
+// log2(100000) == 16.609xxx
+// log2(200000) == 17.609xxx
+// log2(500000) == 18.931xxx
+// log2(1000000) == 19.931xxx
+
+void solve() {
+  cout << "    8888888888  888    88888\n";
+  cout << "   88     88   88 88   88  88\n";
+  cout << "    8888  88  88   88  88888\n";
+  cout << "       88 88 888888888 88   88\n";
+  cout << "88888888  88 88     88 88    888888\n";
+  cout << "\n";
+  cout << "88  88  88   888    88888    888888\n";
+  cout << "88  88  88  88 88   88  88  88\n";
+  cout << "88 8888 88 88   88  88888    8888\n";
+  cout << " 888  888 888888888 88  88      88\n";
+  cout << "  88  88  88     88 88   88888888\n";
+}
+
+void input() {
+  
+}
+
+int main() {
+  fastio;
+
+  input();
+  solve();
+
+  return 0;
+}
