b8f7c0ea0b0b95ee7a49f2afd500b1724b3cef33
2root <2rootcom4@gmail.com>
2022-08-24 21:31:00 +0900

    feat: 18096.cpp in baekjoon/b5

---
+++ baekjoon/1_bronze/level_5/cpp/18096.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MAX_N 302
+#define MAX_LENGTH 5003 >> 1
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+#define SQUARE(x) ((x) * (x))
+#define MOD 1000000007
+typedef long long ll;
+// log2(100000) == 16.609xxx
+// log2(200000) == 17.609xxx
+// log2(500000) == 18.931xxx
+// log2(1000000) == 19.931xxx
+
+int N;
+
+void solve() {
+  cout << 1;
+}
+
+void input() {
+  cin >> N;
+}
+
+int main() {
+  fastio;
+
+  input();
+  solve();
+
+  return 0;
+}
