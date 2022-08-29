f0b07751836d8b69fe8fc7e030538b6e479946e6
2root <2rootcom4@gmail.com>
2022-08-29 16:24:38 +0900

    feat: 10807.cpp in baekjoon/b5

---
+++ baekjoon/1_bronze/level_5/cpp/10807.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MAX_N 100005
+#define MAX_VAL 202
+#define MAX_LENGTH 5003 >> 1
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+#define SQUARE(x) ((x) * (x))
+#define MOD 1000000007
+typedef long long ll;
+// log2(100000) == 16.609xxx
+// log2(200000) == 17.609xxx
+// log2(500000) == 18.931xxx
+// log2(1000000) == 19.931xxx
+
+int N, v;
+int cnt[MAX_VAL];
+
+void solve() {
+  cout << cnt[v + 100];
+}
+
+void input() {
+  cin >> N;
+
+  for (int i = 0; i < N; i++) {
+    cin >> v;
+    cnt[v + 100]++;
+  }
+
+  cin >> v;
+}
+
+int main() {
+  fastio;
+
+  input();
+  solve();
+
+  return 0;
+}
