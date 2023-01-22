507048047a2d7a6e57f7a55bdde4f4545bf2f9c3
RootLEE2 <2rootcom4@gmail.com>
2023-01-22 11:53:01 +0900

    feat: 11382.cpp in baekjoon/b5

---
+++ baekjoon/1_bronze/level_5/cpp/11382.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+	ll A, B, C;
+	cin >> A >> B >> C;
+	cout << A + B + C;
+}
+
+int main()
+{
+	fastio;
+
+	int t = 1;
+	// cin >> t;
+	while (t--) {
+		solve();
+	}
+
+	return 0;
+}
