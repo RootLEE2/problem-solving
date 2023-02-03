557b288cf4a7d526d12005dfb248523e4453667f
RootLEE2 <2rootcom4@gmail.com>
2023-02-03 17:08:08 +0900

    feat: 16394.cpp in baekjoon/b5

---
+++ baekjoon/1_bronze/level_5/cpp/16394.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+	int year;
+	cin >> year;
+	cout << year - 1946;
+}
+
+int main()
+{
+	fastio;
+
+	int t = 1;
+	// cin >> t;
+
+	while (t--) {
+		solve();
+	}
+
+	return 0;
+}
