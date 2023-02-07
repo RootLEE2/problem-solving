68392034785512273278d625e9b2c8e625e56770
RootLEE2 <2rootcom4@gmail.com>
2023-02-07 12:21:27 +0900

    feat: 15962.cpp in baekjoon/b5

---
+++ baekjoon/1_bronze/level_5/cpp/15962.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define NUM_OF_ALPHABET 26
+#define INF 0x3f3f3f3f
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+	cout << "파이팅!!";
+}
+
+int main()
+{
+	fastio;
+
+	int t = 1;
+	// cin >> t;
+
+	while (t--) {
+		solve();
+	}
+
+	return 0;
+}
